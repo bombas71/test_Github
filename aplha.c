a8b7546e001228fa722c0ccb96a66505f369a004
Giannopoulos Evangelos <e.giannopoulos@intralot.com>
2021-07-22 16:11:23 +0300

    updated_comments

--- aplha.c
+++ aplha.c
@@ -4,5 +4,5 @@ int main(){
     printf("Hello World! \n");
     printf("Hello World! \n");
     printf("Hello World! \n");
-    printf("Hello World! \n");
+    printf("Hello World! \n");// me kapoia comments
 }   
