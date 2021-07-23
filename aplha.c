8e358e72d59eb928c4cb539f092a0c2b631eee3e
bombas71 <50680530+bombas71@users.noreply.github.com>
2021-07-23 15:08:19 +0300

    Update aplha.c

--- aplha.c
+++ aplha.c
@@ -3,6 +3,6 @@
 int main(){
     printf("Hello World! \n");
     printf("Hello World! \n");
-    printf("Hello World! \n");
+    printf("Hello World! \n");///comments
     printf("Hello World! \n");// me kapoia comments
 }   
