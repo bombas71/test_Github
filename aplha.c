d5277b4898c61d8898467cfcb73d22fe821ef569
Giannopoulos Evangelos <e.giannopoulos@intralot.com>
2021-07-26 15:35:22 +0300

    Some updates

--- aplha.c
+++ aplha.c
@@ -5,4 +5,5 @@ int main(){
     printf("Hello World! \n");///clone_branch
     printf("Hello World! \n");///comments
     printf("Hello World! \n");// me kapoia comments
+    printf("Hello World! \n");// clone_branch
 }   
